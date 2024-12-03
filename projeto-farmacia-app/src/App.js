6e2cffc6719ee56cc8dad706d59b804d1e0f7a72
DinossauroProductions <guilhermeovergame@gmail.com>
2024-12-03 11:41:59 -0300

    Criar projeto react

---
+++ projeto-farmacia-app/src/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className='App'>
+      <Button variant='contained'>Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
